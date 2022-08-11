093022f2a70dcebefd741e9b4017d0d8e13fa4bb
Ireshaprabodhani <84035631+Ireshaprabodhani@users.noreply.github.com>
2022-08-12 03:41:27 +0530

    create MyNotes.js

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+
+const footer = () => {
+  return (
+      <footer
+          style={{
+          
+              width: "100%",
+              position: "relative",
+              bottom: 0,
+              display: "flex",
+              justifyContent : "center",
+
+          }}
+      >
+          <Container>
+              <Row>
+                  <Col className='text-center py-3'>
+                   Copyright @copy; Student Notes  
+                  </Col>
+              </Row>  
+
+          </Container>
+
+    </footer>
+  )
+}
+
+export default footer;
---
+++ frontend/src/data/notes.js
@@ -0,0 +1,18 @@
+const notes = [
+  {
+    _id: "1",
+    title: "ggyhuhfuf",
+    content:
+      "Exclusive vouchersBetter deal Personalised recommendationsEarly accessdguw",
+    category: "ireshaPP",
+  },
+
+  {
+    _id: "2",
+    title: "ggyhuhfuf",
+    content: "xcvdguw",
+    category: "tharu",
+  },
+];
+
+module.exports = notes;
