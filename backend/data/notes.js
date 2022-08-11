2d7b7050b0d66263dc66bf71ba017cffbfe37282
Ireshaprabodhani <84035631+Ireshaprabodhani@users.noreply.github.com>
2022-08-11 10:00:54 +0530

    create backend part and install react

---
+++ backend/data/notes.js
@@ -0,0 +1,14 @@
+const notes = [
+  {
+    _id: "1",
+    tittle: "ggyhuhfuf",
+    content: "xcvdguw",
+  },
+
+  {
+    _id: "2",
+    tittle: "ggyhuhfuf",
+    content: "xcvdguw",
+  },
+];
+module.exports = notes;
